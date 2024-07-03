a3b74d1bb491f4739890f351ec895f93101c6c1a
kang <kang@robots.ox.ac.uk>
2024-07-03 16:51:40 +0100

    [WIP] Work In Progress, repeat the order book

--- lob/lob_seq_model.py
+++ lob/lob_seq_model.py
@@ -422,6 +422,14 @@ class PaddedLobPredModel(nn.Module):
         # repeat book input to match message length
         x_b = jnp.repeat(x_b, x_m.shape[0] // x_b.shape[0], axis=0)
         
+        token_index = # TODO
+
+        # Calculate the repeat counts for each segment
+        K = x_m.shape[0] // x_b.shape[0]
+        repeats = [K - token_index] + [K] * (x_b.shape[0] - 2) + [token_index]
+        x_b = jnp.concatenate([jnp.repeat(x_b[i:i+1], repeats[i], axis=0) for i in range(x_b.shape[0])], axis=0)
+        
+            
         x = jnp.concatenate([x_m, x_b], axis=1)
         # TODO: again, check integration time steps make sense here
         x = self.fused_s5(x, jnp.ones(x.shape[0]))
