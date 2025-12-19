c31c42878a4c8a9de0cc1be30d50094ed3df30ad
isambard <kang@robots.ox.ac.uk>
2025-12-19 23:07:35 +0000

    [FEAT] multiple token  mode

--- lob/dataloading.py
+++ lob/dataloading.py
@@ -27,6 +27,7 @@ def create_lobster_prediction_dataset(
 		use_simple_book: bool = False,
 		book_transform: bool = False,
 		book_depth: int = 500,
+		token_mode: int = 22,
 		test_dir_name: Union[str, Path, None] = None,
 		n_data_workers: int = 0,
 		return_raw_msgs: bool = False,
@@ -54,6 +55,7 @@ def create_lobster_prediction_dataset(
 		use_simple_book=use_simple_book,
 		book_transform=book_transform,
 		book_depth=book_depth,
+		token_mode=token_mode,
 		test_data_dir=test_dir_name,
 		n_cache_files=250,  # large number to keep everything in cache
 		return_raw_msgs=return_raw_msgs,
